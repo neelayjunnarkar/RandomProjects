bf2d1e1f7460442533c3d7a7c92367997a2ecda0
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2017-01-23 13:07:20 -0800

    changed main to take args from console

--- BigInt/main.cpp
+++ BigInt/main.cpp
@@ -1,33 +1,48 @@
 #include <map>
 #include "BigInt.hpp"
 #include <functional>
-
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
 
 int main(int argc, char **argv) {
-	if (argc != 2 && argc != 4) {
-		std::cerr << "exec in format: <exe> <num>" << std::endl;
-		std::cerr << "OR" << std::endl;
-		std::cerr << "exec in format: <exe> <num1> <plus or minus> <num2>" << std::endl;
-		return 1;
-	}
-	if (argc == 2) {
-		BigInt a{argv[1]};
-		cout << "a: " << a << "    " << a.represent() << endl;
-	} else if (argc == 4) {
-		std::map<string, std::function<void()>> fns;
-		BigInt a(argv[1]);
-		BigInt b(argv[3]);
-		cout << "a: " << a << "    " << a.represent() << endl;
-		cout << "b: " << b << "    " << b.represent() << endl;
-		cout << boolalpha << endl;
-		fns["+"] = [&](){ cout << "a + b = " << a+b << "    " << (a+b).represent() << endl; };
-		fns["-"] = [&](){ cout << "a - b = " << a-b << "    " << (a-b).represent() << endl; };
-		fns["gt"] = [&]() { cout << "a > b = " << (a > b) << endl; };
-		fns["lte"] = [&]() { cout << "a <= b = " << (a <= b)  << endl; };
-		fns["lt"] = [&]() { cout << "a < b = " << (a < b) << endl; };
-		fns["gte"] = [&]() { cout << "a >= b = " << (a >= b) << endl; };
-		fns["=="] = [&]() { cout << "a == b = " << (a == b) << endl; };
-		fns["!="] = [&]() { cout << "a != b = " << (a != b) << endl; };
-		fns[argv[2]]();
+	while (true) {
+		// Take in a line of input and split it into tokens
+		string line;
+		getline(cin, line);
+		istringstream iss(line);
+		vector<string> args;
+		copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(args));						
+	
+		if (args.size() != 1 && args.size() != 3) {
+			std::cerr << "enter in format: <num>" << std::endl;
+			std::cerr << "OR" << std::endl;
+			std::cerr << "in format: <num1> <operation> <num2>" << std::endl;
+			continue;
+		}
+		if (args.size() == 1) {
+			if (args[0] == "q")
+				break;
+			BigInt a{args[0]};
+			cout << "a: " << a << "    " << a.represent() << endl;
+		} else if (args.size() == 3) {
+			std::map<string, std::function<void()>> fns;
+			BigInt a(args[0]);
+			BigInt b(args[2]);
+			cout << "a: " << a << "    " << a.represent() << endl;
+			cout << "b: " << b << "    " << b.represent() << endl;
+			cout << boolalpha;
+			fns["+"] = [&](){ cout << "a + b = " << a+b << "    " << (a+b).represent() << endl; };
+			fns["-"] = [&](){ cout << "a - b = " << a-b << "    " << (a-b).represent() << endl; };
+			fns[">"] = [&]() { cout << "a > b = " << (a > b) << endl; };
+			fns["<="] = [&]() { cout << "a <= b = " << (a <= b)  << endl; };
+			fns["<"] = [&]() { cout << "a < b = " << (a < b) << endl; };
+			fns[">="] = [&]() { cout << "a >= b = " << (a >= b) << endl; };
+			fns["=="] = [&]() { cout << "a == b = " << (a == b) << endl; };
+			fns["!="] = [&]() { cout << "a != b = " << (a != b) << endl; };
+			fns[args[1]]();
+		}
+		cout << endl;
 	}
 }
