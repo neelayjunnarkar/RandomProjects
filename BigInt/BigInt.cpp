4e0109d7e0c6c7130ed95a3f99a1fc22addb1515
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2017-01-24 11:32:14 -0800

    Fixed bounds overflow in _pos_pos_diff and added exceptions

--- BigInt/BigInt.cpp
+++ BigInt/BigInt.cpp
@@ -3,59 +3,73 @@
 #include <algorithm>
 #include <cmath>
 #include <numeric>
+#include <cassert>
+#include <cctype>
+#include <exception>
 
-BigInt::BigInt(const BigInt &b):
+#define DEBUG(TEST, MESSAGE) if ((TEST)) { throw std::exception(MESSAGE); }
+
+bool is_integer(const string &str) {
+	for(const auto &digit : str) {
+		if(!isdigit(digit))
+			return false;
+	}
+	return true;
+}
+
+BigInt::BigInt(const BigInt &b) :
 	_data{b._data},
 	_sign{b._sign} {
-	
 }
 
 BigInt::BigInt(string _n) {
-	if (_n == "")
+	if(_n == "")
 		return;
-	if (_n == "0") {
+	if(_n == "0") {
 		_data.push_back(0);
 		return;
 	}
-	if (_n[0] == '-') {
+	if(_n[0] == '-') {
 		_sign = -1;
-		_n = _n.substr(1, _n.length()-1);
+		_n = _n.substr(1, _n.length() - 1);
 	}
+	DEBUG(!is_integer(_n), "BigInt cstor: Input is neither a blank string nor an integer");
+
 	int right = _n.length();
-	int left = max(right-max_bucket_digits, 0);
+	int left = max(right - max_bucket_digits, 0);
 	string str;
-	while (right != left) {
-		str = _n.substr(left, right-left);
+	while(right != left) {
+		str = _n.substr(left, right - left);
 		right = left;
-		left = max(right-max_bucket_digits, 0);
+		left = max(right - max_bucket_digits, 0);
 		_data.push_back(stol(str));
 	}
 }
 
 BigInt BigInt::operator + (BigInt other) {
-	if (this->_sign == 1 && other._sign == 1)
-		return _pos_pos_sum(*this, other);	
-	if (this->_sign == -1 && other._sign == -1)
+	if(this->_sign == 1 && other._sign == 1)
+		return _pos_pos_sum(*this, other);
+	if(this->_sign == -1 && other._sign == -1)
 		return -(-*this + -other);
-	if (this->_sign == 1 && other._sign == -1)
+	if(this->_sign == 1 && other._sign == -1)
 		return *this - -other;
-	if (this->_sign == -1 && other._sign == 1)
-		return other - -*this;	
+	if(this->_sign == -1 && other._sign == 1)
+		return other - -*this;
 	return BigInt();
 }
 
 BigInt BigInt::operator - (BigInt other) {
-	if (this->_sign == 1 && other._sign == -1) {
+	if(this->_sign == 1 && other._sign == -1) {
 		return *this + -other;
-	} else if (this->_sign == -1 && other._sign == 1) {
+	} else if(this->_sign == -1 && other._sign == 1) {
 		return -(-*this + other);
-	} else if (this->_sign == 1 && other._sign == 1) {
-		if (*this > other)
+	} else if(this->_sign == 1 && other._sign == 1) {
+		if(*this > other)
 			return _pos_pos_diff(*this, other);
-		if (other > *this)
+		if(other > *this)
 			return -_pos_pos_diff(other, *this);
 		return BigInt();
-	} else if (this->_sign == -1 && other._sign == -1) {
+	} else if(this->_sign == -1 && other._sign == -1) {
 		return *this + -other;
 	}
 	return BigInt();
@@ -68,63 +82,72 @@ BigInt BigInt::operator - () {
 }
 
 BigInt BigInt::_pos_pos_sum(BigInt a, BigInt b) {
-		BigInt res{""};
-		int max_i = max(b._data.size(), a._data.size());
-		res._data.resize(max_i);
-		int carry = 0;
-		for (int i = 0; i < max_i; ++i) {
-			int a_val = (i >= a._data.size()) ? 0 : a._data[i];
-			int b_val = (i >= b._data.size()) ? 0 : b._data[i];
-			int sum = a_val + b_val + carry;
-			int res_val = sum%max_bucket_val;
-			carry = sum/max_bucket_val;
-			res._data[i] = res_val;
-		}
-		if (carry != 0)
-			res._data.push_back(carry);
-		// Trim _data so no leading zeros
-		while (res._data.back() == 0)
-			res._data.pop_back();
-		res._data.shrink_to_fit();
-		return res;	
+	DEBUG(a < BigInt{"0"} || b < BigInt{"0"}, "_pos_pos_sum: either a or b is not greater than zero");
+
+	BigInt res{""};
+	int max_i = max(b._data.size(), a._data.size());
+	res._data.resize(max_i);
+	int carry = 0;
+	for(int i = 0; i < max_i; ++i) {
+		int a_val = (i >= a._data.size()) ? 0 : a._data[i];
+		int b_val = (i >= b._data.size()) ? 0 : b._data[i];
+		int sum = a_val + b_val + carry;
+		int res_val = sum%max_bucket_val;
+		carry = sum / max_bucket_val;
+		res._data[i] = res_val;
+	}
+	if(carry != 0)
+		res._data.push_back(carry);
+	// Trim _data so no leading zeros
+	while(res._data.back() == 0)
+		res._data.pop_back();
+	res._data.shrink_to_fit();
+	return res;
 }
 BigInt BigInt::_pos_pos_diff(BigInt a, BigInt b) {
-	int max_i = max(a._data.size(), b._data.size())-1; //cuz want to deal with end separately
-	for (int i = 0; i < max_i; ++i) {
-		if (a._data[i] >= b._data[i]) {
-			a._data[i] -= b._data[i];
-		} else if (b._data[i] > a._data[i]) {
-			a._data[i] = max_bucket_val + a._data[i] - b._data[i];
-			a._data[i+1] -= 1;
+	DEBUG(a < BigInt{"0"} || b < BigInt{"0"}, "_pos_pos_diff: either a or b is not greater than zero");
+	DEBUG(a < b, "_pos_pos_diff: a is not greater than b");
+
+	int max_i = max(a._data.size(), b._data.size()) - 1; //cuz want to deal with end separately
+	for(int i = 0; i < max_i; ++i) {
+		int a_val = (i >= a._data.size()) ? 0 : a._data[i];
+		int b_val = (i >= b._data.size()) ? 0 : b._data[i];
+		if(a_val >= b_val) {
+			a._data[i] -= b_val;
+		} else if(b_val > a_val) {
+			a._data[i] = max_bucket_val + a_val - b_val;
+			a._data[i + 1] -= 1;
 		}
 	}
-	if (a._data[max_i] >= b._data[max_i]) {
-		a._data[max_i] -= b._data[max_i];
-	} else if (b._data[max_i] > a._data[max_i]) {
-		a._data[max_i] = b._data[max_i] - a._data[max_i];
+	int a_val = (max_i >= a._data.size()) ? 0 : a._data[max_i];
+	int b_val = (max_i >= b._data.size()) ? 0 : b._data[max_i];
+	if(a_val >= b_val) {
+		a._data[max_i] -= b_val;
+	} else if(b_val > a_val) {
+		a._data[max_i] = b_val - a_val;
 		a._sign = -1;
 	}
-	while (a._data.back() == 0)
+	while(a._data.back() == 0)
 		a._data.pop_back();
 	a._data.shrink_to_fit();
 	return a;
 }
 
 bool BigInt::operator > (BigInt other) {
-	if (this->_sign == 1 && other._sign == -1)
+	if(this->_sign == 1 && other._sign == -1)
 		return true;
-	if (this->_sign == -1 && other._sign == 1)
+	if(this->_sign == -1 && other._sign == 1)
 		return false;
-	if (this->_sign == -1 && other._sign == -1)
+	if(this->_sign == -1 && other._sign == -1)
 		return -*this < -other;
-	if (this->_data.size() > other._data.size())
+	if(this->_data.size() > other._data.size())
 		return true;
-	if (this->_data.size() < other._data.size())
+	if(this->_data.size() < other._data.size())
 		return false;
-	for (int i = this->_data.size()-1; i >= 0; --i) {
-		if (this->_data[i] > other._data[i])
+	for(int i = this->_data.size() - 1; i >= 0; --i) {
+		if(this->_data[i] > other._data[i])
 			return true;
-		if (this->_data[i] < other._data[i])
+		if(this->_data[i] < other._data[i])
 			return false;
 	}
 	return false; //are equal
@@ -135,20 +158,20 @@ bool BigInt::operator <= (BigInt other) {
 }
 
 bool BigInt::operator < (BigInt other) {
-	if (this->_sign == -1 && other._sign == 1)
+	if(this->_sign == -1 && other._sign == 1)
 		return true;
-	if (this->_sign == 1 && other._sign == -1)
+	if(this->_sign == 1 && other._sign == -1)
 		return false;
-	if (this->_sign == -1 && other._sign == -1)
+	if(this->_sign == -1 && other._sign == -1)
 		return -*this > -other;
-	if (this->_data.size() < other._data.size())
+	if(this->_data.size() < other._data.size())
 		return true;
-	if (this->_data.size() > other._data.size())
+	if(this->_data.size() > other._data.size())
 		return false;
-	for (int i = this->_data.size()-1; i >= 0; --i) {
-		if (this->_data[i] < other._data[i])
+	for(int i = this->_data.size() - 1; i >= 0; --i) {
+		if(this->_data[i] < other._data[i])
 			return true;
-		if (this->_data[i] > other._data[i])
+		if(this->_data[i] > other._data[i])
 			return false;
 	}
 	return false; //are equal
@@ -159,12 +182,12 @@ bool BigInt::operator >= (BigInt other) {
 }
 
 bool BigInt::operator == (BigInt other) {
-	if (this->_sign != other._sign)
+	if(this->_sign != other._sign)
 		return false;
-	if (this->_data.size() != other._data.size())
+	if(this->_data.size() != other._data.size())
 		return false;
-	for (int i = this->_data.size()-1; i >= 0; --i) {
-		if (this->_data[i] != other._data[i])
+	for(int i = this->_data.size() - 1; i >= 0; --i) {
+		if(this->_data[i] != other._data[i])
 			return false;
 	}
 	return true;
@@ -175,7 +198,7 @@ bool BigInt::operator != (BigInt other) {
 }
 
 string BigInt::represent() const {
-	string representation = accumulate(_data.begin(), _data.end(), string{}, [](string str, int n) { return str + string{","}  + ::to_string(n); });
+	string representation = accumulate(_data.begin(), _data.end(), string{}, [] (string str, int n) { return str + string{","}  +::to_string(n); });
 	string sign = _sign == 1 ? string{""} : string{"-"};
 	return sign + representation;
 }
@@ -183,14 +206,14 @@ string BigInt::represent() const {
 string BigInt::to_string() const {
 	auto temp = _data;
 	reverse(temp.begin(), temp.end());
-	auto num_to_string = [](int n) -> string {
+	auto num_to_string = [] (int n) -> string {
 		string str = ::to_string(n);
 		string pad = "";
-		for (int i = 0; i < max_bucket_digits-str.length(); ++i) 
+		for(int i = 0; i < max_bucket_digits - str.length(); ++i)
 			pad += "0";
 		return pad + str;
 	};
-	string str = ::to_string(*temp.begin()) + accumulate(temp.begin()+1, temp.end(), string{}, [&](string str, int n) { return str + num_to_string(n); });
+	string str = ::to_string(*temp.begin()) + accumulate(temp.begin() + 1, temp.end(), string{}, [&] (string str, int n) { return str + num_to_string(n); });
 	string sign = _sign == 1 ? string{""} : string{"-"};
 	return sign + str;
 }
--- BigInt/main.cpp
+++ BigInt/main.cpp
@@ -1,5 +1,6 @@
-#include <map>
 #include "BigInt.hpp"
+
+#include <map>
 #include <functional>
 #include <iostream>
 #include <string>
@@ -7,42 +8,49 @@
 #include <iterator>
 
 int main(int argc, char **argv) {
-	while (true) {
-		// Take in a line of input and split it into tokens
-		string line;
-		getline(cin, line);
-		istringstream iss(line);
-		vector<string> args;
-		copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(args));						
-	
-		if (args.size() != 1 && args.size() != 3) {
-			std::cerr << "enter in format: <num>" << std::endl;
-			std::cerr << "OR" << std::endl;
-			std::cerr << "in format: <num1> <operation> <num2>" << std::endl;
-			continue;
+
+	while(true) {
+		try {
+			// Take in a line of input and split it into tokens
+			string line;
+			getline(cin, line);
+			istringstream iss(line);
+			vector<string> args;
+			copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(args));
+
+			if(args.size() != 1 && args.size() != 3) {
+				std::cerr << "enter in format: <num>" << std::endl;
+				std::cerr << "OR" << std::endl;
+				std::cerr << "in format: <num1> <operation> <num2>" << std::endl;
+				continue;
+			}
+			if(args.size() == 1) {
+				if(args[0] == "q")
+					break;
+				BigInt a{args[0]};
+				cout << "a: " << a << "    " << a.represent() << endl;
+			} else if(args.size() == 3) {
+				std::map<string, std::function<void()>> fns;
+				BigInt a(args[0]);
+				BigInt b(args[2]);
+				cout << "a: " << a << "    " << a.represent() << endl;
+				cout << "b: " << b << "    " << b.represent() << endl;
+				cout << boolalpha;
+				fns["+"]  = [&] () { cout << "a + b = " << a + b << "    " << (a + b).represent() << endl; };
+				fns["-"]  = [&] () { cout << "a - b = " << a - b << "    " << (a - b).represent() << endl; };
+				fns[">"]  = [&] () { cout << "a > b = " << (a > b) << endl; };
+				fns["<="] = [&] () { cout << "a <= b = " << (a <= b) << endl; };
+				fns["<"]  = [&] () { cout << "a < b = " << (a < b) << endl; };
+				fns[">="] = [&] () { cout << "a >= b = " << (a >= b) << endl; };
+				fns["=="] = [&] () { cout << "a == b = " << (a == b) << endl; };
+				fns["!="] = [&] () { cout << "a != b = " << (a != b) << endl; };
+				fns[args[1]]();
+			}
+			cout << endl;
+		} catch(std::exception &e) {
+			cerr << e.what() << endl;
+			cout << endl;
 		}
-		if (args.size() == 1) {
-			if (args[0] == "q")
-				break;
-			BigInt a{args[0]};
-			cout << "a: " << a << "    " << a.represent() << endl;
-		} else if (args.size() == 3) {
-			std::map<string, std::function<void()>> fns;
-			BigInt a(args[0]);
-			BigInt b(args[2]);
-			cout << "a: " << a << "    " << a.represent() << endl;
-			cout << "b: " << b << "    " << b.represent() << endl;
-			cout << boolalpha;
-			fns["+"] = [&](){ cout << "a + b = " << a+b << "    " << (a+b).represent() << endl; };
-			fns["-"] = [&](){ cout << "a - b = " << a-b << "    " << (a-b).represent() << endl; };
-			fns[">"] = [&]() { cout << "a > b = " << (a > b) << endl; };
-			fns["<="] = [&]() { cout << "a <= b = " << (a <= b)  << endl; };
-			fns["<"] = [&]() { cout << "a < b = " << (a < b) << endl; };
-			fns[">="] = [&]() { cout << "a >= b = " << (a >= b) << endl; };
-			fns["=="] = [&]() { cout << "a == b = " << (a == b) << endl; };
-			fns["!="] = [&]() { cout << "a != b = " << (a != b) << endl; };
-			fns[args[1]]();
-		}
-		cout << endl;
 	}
+
 }
